4a2588a6a7fe65bc36213dc5a5d416f2ed25c2d5
Rennan <raljs@ecomp.poli.br>
2023-07-21 21:33:24 -0300

    Busca em largura e profundidade

--- Graph.java
+++ Graph.java
@@ -1,11 +1,27 @@
 public class Graph {
 
-    public Line lines[];
-    public Vertice vertices[];
+    private Line lines[];
+    private Vertice vertices[];
 
     public void graphInit(int len) {
 
         lines = new Line[len];
 
+    }
+
+    public Line[] getLines() {
+        return this.lines;
+    }
+
+    public void setLines(Line[] lines) {
+        this.lines = lines;
+    }
+
+    public Vertice[] getVertices() {
+        return this.vertices;
+    }
+
+        public void setVertices(Vertice[] vertices) {
+        this.vertices = vertices;
     }
 }
--- Vertice.java
+++ Vertice.java
@@ -1,3 +1,16 @@
 public class Vertice {
-    
+
+    private String nome;
+
+    public Vertice(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
