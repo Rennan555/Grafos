98de1c63c3bdbf7339b3ca24f0a874dc271106f6
RENNAN DE ABREU E LIMA JANUARIO SILVA <raeljs@Poli.local>
2023-08-16 10:19:11 -0300

    Implementação incolpleta de Sudoku

---
+++ Sudoku.java
@@ -0,0 +1,21 @@
+
+
+public class Sudoku {
+
+    int[][] grade = new int[9][9];
+
+    public boolean isValid() {
+
+        for(int i=0;i<9;i++) {
+
+            for(int j=0;j<9;j++) {
+
+                if(grade)
+
+            }
+
+        }
+
+    }
+
+}
